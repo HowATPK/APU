e43083859650d0a8cce0b338881592355273a256
artur <you@example.com>
2021-11-17 22:53:04 +0100

    add lab2

---
+++ Zad2.R
@@ -0,0 +1,9 @@
+library(data.tree)
+library(ahp)
+setwd(C:\Users\artur\Desktop\studia\ApuSprawko\Lab2)
+drzewo <- Load("macbook.yml");
+print(drzewo)
+Calculate(drzewo)
+Visualize(drzewo)
+Analyze(drzewo)
+AnalyzeTable(drzewo)
