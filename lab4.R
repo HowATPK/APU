e05dafde89585db61302cf7e6d84746e6795084f
Artur Hałat <qwe>
2022-01-09 14:12:44 +0100

    changes after seeing mistake in lab4 commands

--- lab4.R
+++ lab4.R
@@ -3,7 +3,7 @@ install.packages("C50")
 library("C50")
 macbook <- read.csv('macbook.csv')
 View(macbook)
-drzewo<- C5.0(x=macbook[,-6], y=factor(macbook$factors))
+drzewo<- C5.0(x=macbook[,-8], y=factor(macbook$factors))
 drzewo
 summary(drzewo)
 plot(drzewo)
